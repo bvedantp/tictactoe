6b3e08b5add278be9d80db1a41ddb87703f99468
B Vedant Pandey <bvedantp@gmail.com>
2021-10-18 15:39:58 +0530

    Implemented addMark

--- logic.js
+++ logic.js
@@ -1,12 +1,35 @@
+let board = document.getElementsByClassName("game-board");
+
+
+
 let gameBoard = (function() {
     let game = []
     return {game};
 })();
 
-const playerFactory = function(name) {
+const player = function(name) {
+    
     return {name};
 };
 
-let gameFlow = (function() {
-    //currentPlayer
+let game = (function() {
+    let addMark = (mark) => {
+        for(let i=0;i<board.length;i++){
+            board[i].addEventListener('click',(e) => {
+                e.target.textContent= `${mark}`;
+            })
+        }         
+    }
+    return {addMark};
 })();
+
+game.addMark('O'); //change X or O
+
+function boardFill(game) {
+    let cells = document.querySelectorAll(".cell")
+    for(let i=0;i<game.length;i++) {
+        cells[i].innerHTML = `${game[i]}`; 
+    }
+}
+
+boardFill(gameBoard.game);
